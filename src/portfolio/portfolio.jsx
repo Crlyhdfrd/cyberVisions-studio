16ed91f899cfb78f8e05a5eb85007c2c80948796
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-05-24 23:19:39 -0500

    styled the portfolio component added icons images and links for projects

--- src/portfolio/portfolio.jsx
+++ src/portfolio/portfolio.jsx
@@ -1,5 +1,7 @@
 import "./portfolio.scss";
 import portfolioData from "./portfolioData";
+import GitHubIcon from "../assets/github.svg?react";
+import LinkIcon from "../assets/link.svg?react";
 
 function About() {
   return (
@@ -16,11 +18,20 @@ function About() {
 
 function PortItem({ key, project }) {
   return (
-    <div key={key}
-      className="PortItem">
-      <img className="PortImage" src={project.image} alt={project.title} />
+    <div key={key} className="PortItem">
+      <div className="PortImageContainer">
+        <img className="PortImage" src={project.image} alt={project.title} />
+      </div>
       <h3 className="PortTitle">{project.title}</h3>
       <p className="PortSubtitle">{project.subtitle}</p>
+      <div className="PortIcons">
+        <a className="PortIcon" href={project.github} target="_blank">
+          <GitHubIcon />
+        </a>
+        <a className="PortIcon" href={project.live} target="_blank">
+          <LinkIcon />
+        </a>
+      </div>
     </div>
   );
 } 
