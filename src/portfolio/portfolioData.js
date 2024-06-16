6d232869c6c359ae2166bc247bebf9e3a8eb6c96
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-06-15 19:48:35 -0500

    added images and completed styling

--- src/portfolio/portfolioData.js
+++ src/portfolio/portfolioData.js
@@ -1,51 +1,51 @@
 const portfolioData = [
   {
-    title: "project 1",
+    title: "LOTR Wiki",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "/images/sunset-4086848_1920.jpg",
+      "A Lord of the Rings wiki that allows you to search for characters, locations, and events from the Lord of the Rings series. It also allows you to view information about the characters, locations, and events.",
+    image: "public/images/frodo landing ring.gif",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://memiscavage.github.io/LOTR-wiki/#gsc.tab=0",
   },
   {
-    title: "project 2",
+    title: "GW Weather Dashboard",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "https://picsum.photos/400/300",
+      "A weather dashboard that allows you to search for a city and get the current weather and a 5-day forecast. It also saves your search history so you can easily access the weather for cities you have searched for in the past.",
+    image: "/images/3700914493.png",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://crlyhdfrd.github.io/GW-weather-dashboard/",
   },
   {
-    title: "project 3",
+    title: "Personal Daybook",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "https://picsum.photos/400/300",
+      "A personal day book application that keeps track of your daily activities and events. It allows you to add, edit, and delete events. It also allows you to search for events by date.",
+    image: "/images/melanfolia--c1k47H1Reg-unsplash.jpg",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://crlyhdfrd.github.io/Personal-Daybook/",
   },
   {
-    title: "project 4",
+    title: "Cornell Note Taker",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "https://picsum.photos/400/300",
+      "A note taking application that allows you to take notes in the Cornell note taking format. It also allows you to save your notes to your local storage so you can access them later.",
+    image: "/images/david-travis-5bYxXawHOQg-unsplash.jpg",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://crlyhdfrd.github.io/cornell-note-taker/",
   },
   {
-    title: "project 5",
+    title: "Password Generator",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "https://picsum.photos/400/300",
+      "A password generator that allows you to generate a random password based on the criteria you select. You can choose the length of the password and whether you want to include uppercase letters, lowercase letters, numbers, and special characters.",
+    image: "/images/kenny-eliason-mgYAR7BzBk4-unsplash.jpg",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://crlyhdfrd.github.io/password-generator/",
   },
   {
-    title: "project 6",
+    title: "Mudi",
     subtitle:
-      "lorem ipsum dolar sit amet consectetur jack something of another just neeed some text",
-    image: "https://picsum.photos/400/300",
+      "A mood tracker application that allows you to track your mood over time. You can add moods, view your mood history, and see your mood trends over time.",
+    image: "/images/Screenshot 2024-06-15 at 7.25.26 PM.png",
     github: "https://github.com/Crlyhdfrd",
-    live: "https://www.google.com/",
+    live: "https://mood-tracker-gp-a6fcbb48bcc3.herokuapp.com/",
   },
 ];
 
--- src/resume/resume.jsx
+++ src/resume/resume.jsx
@@ -7,7 +7,18 @@ function Resume() {
       <div className="ResContainer">
         <div className="ResSummaryContainer">
           <p className="ResSummary">
-            I am a full stack web developer with a background in customer service. I have a passion for problem solving and a love for learning new technologies. I am a quick learner and a team player. I am looking for a position where I can use my skills to create innovative and user-friendly applications. I am proficient in HTML, CSS, Javascript, and React. I have experience with Node.js, Express, MySQL, MongoDB, and RESTful APIs. I have a strong understanding of responsive design and accessibility. I am a creative thinker and a detail-oriented developer. I am excited to bring my skills to a team that values collaboration and creativity. I am eager to grow as a developer and to contribute to projects that make a positive impact. I am looking forward to working with a team that is passionate about creating high-quality applications, dedicated to creating innovative and user-friendly applications, values creativity and collaboration. 
+            I am a full stack web developer with a background in customer service.
+            I have a passion for problem solving and a love for learning new technologies.
+            I am a quick learner and a team player.
+            I am looking for a position where I can use my skills to create innovative and user-friendly applications.
+            I am proficient in HTML, CSS, Javascript, and React.
+            I have experience with Node.js, Express, MySQL, MongoDB, and RESTful APIs.
+            I have a strong understanding of responsive design and accessibility.
+            I am a creative thinker and a detail-oriented developer.
+            I am excited to bring my skills to a team that values collaboration and creativity.
+            I am eager to grow as a developer and to contribute to projects that make a positive impact.
+            I am looking forward to working with a team that is passionate about creating high-quality applications,
+            dedicated to creating innovative and user-friendly applications, values creativity and collaboration.
           </p>
           <button className="Download">Download</button>
         </div>
